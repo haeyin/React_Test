607fceeb592a52008e3bf69321a5b820194241be
haeyin <haeyin.shin@gmail.com>
2021-02-28 23:05:14 -0800

    Basic testing

---
+++ reacttest001/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer> This is a footer, copyright 2021.</footer>
+    )
+}
+
+export default Footer
---
+++ reacttest001/src/Components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <div>
+            This is the main content that is supposed to be super important.
+        </div>
+    )
+}
+
+export default MainContent
